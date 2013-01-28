c8aaff257ac0a2804dd5aaf56dfebee525dd6838
Giancarlo Inductivo <gian@dynamicobjx.com>
2013-01-28 12:22:05 +0800

    Migrating to play 2.1 RC2, needed to turn off javadoc due to publishing issue (http://bit.ly/Vfmo5Q)

--- module/project/Build.scala
+++ module/project/Build.scala
@@ -1,19 +1,19 @@
 import sbt._
 import Keys._
-import PlayProject._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
     val appName         = "pdf"
-    val appVersion      = "0.3.1"
+    val appVersion      = "0.3.2-SNAPSHOT"
 
     val appDependencies = Seq(
       "org.xhtmlrenderer" % "core-renderer" % "R8",
         "net.sf.jtidy" % "jtidy" % "r938"
 	)
 
-    val main = PlayProject(appName, appVersion, appDependencies).settings(
-      // Add your own project settings here      
+    val main = play.Project(appName, appVersion, appDependencies).settings(
+      publishArtifact in(Compile, packageDoc) := false //workaround for publishing bug (http://bit.ly/Vfmo5Q)
     )
 
 }
